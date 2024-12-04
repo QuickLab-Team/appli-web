402c2f6f84fcb8bdbf712aa1d8a9508f973398ba
IZUNA-coder <80841824+IZUNA-coder@users.noreply.github.com>
2024-12-04 17:48:38 +0100

    Amélioration de la page de connexion et ajout de la déconnexion

--- apps/utilisateurs/urls.py
+++ apps/utilisateurs/urls.py
@@ -3,8 +3,8 @@ from . import views
 from .views import ConnexionView
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('accueil', views.accueil, name='accueil'),
     path('inscription/', views.inscription, name='inscription'),
-    path('connexion/', ConnexionView.as_view(), name='connexion'),
+    path('', ConnexionView.as_view(), name='connexion'),
+    path('deconnexion/', views.deconnexion, name='deconnexion'),
 ]
--- apps/utilisateurs/views.py
+++ apps/utilisateurs/views.py
@@ -1,9 +1,8 @@
 # preparateurs/views.py
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
-
 
 def home(request):
     return render(request, 'base.html', {
@@ -11,9 +10,13 @@ def home(request):
     })
 
 def accueil(request):
-    return render(request, 'accueil.html', {
-        'titre': '',
-    })
+
+    if request.user.is_authenticated:
+        return render(request, 'utilisateurs/accueil.html', {
+            'titre': 'QuickLab',
+        })
+    else:
+        return redirect('connexion')
 
 def produits(request):
     produits = Produit.objects.all()
@@ -36,5 +39,16 @@ def inscription(request):
         form = UserCreationForm()
     return render(request, 'preparateurs/inscription.html', {'form': form})
 
+
+def deconnexion(request):
+
+    logout(request)
+    return redirect('connexion')
+
 class ConnexionView(LoginView):
-    template_name = 'connexion.html'
+    template_name = 'utilisateurs/connexion.html'
+    success_url = 'accueil'
+    redirect_authenticated_user = False
+
+    def get_success_url(self):
+        return self.success_url
