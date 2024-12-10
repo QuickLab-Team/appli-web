bead5ab2850bd48c15643094ae4e7c5460971c99
Kevin-LB <144025457+Kevin-LB@users.noreply.github.com>
2024-12-10 10:35:59 +0100

    init de la partie reservation

--- apps/reservations/views.py
+++ apps/reservations/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def reservations(request):
+    reservations = Reservation.objects.all()
+    return render(request, 'reservations/etudiants/reservations.html', {
+        'titre': 'QuickLab',
+        'reservations': reservations
+    })
