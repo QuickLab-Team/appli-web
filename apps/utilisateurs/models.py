10730ab4c4d31b2bdecbee72f09ad389b8ba93b3
IZUNA-coder <80841824+IZUNA-coder@users.noreply.github.com>
2025-01-07 15:40:26 +0100

    Refactor Utilisateur model: update fields and choices, improve admin registration

--- apps/utilisateurs/models.py
+++ apps/utilisateurs/models.py
@@ -5,33 +5,33 @@ from .managers import CustomUtilisateurManager
 # Create your models here.
 
 class Utilisateur(AbstractUser):
-    """
-    Utilisateur
-    """
     ROLES = [
-        ('utilisateur', 'Utilisateur'),
-        ('preparateur', 'Préparateur'),
+        ('etudiant', 'Etudiant'),
+        ('preparateur', 'Preparateur'),
         ('administrateur', 'Administrateur'),
     ]
     
+    ANNEES = [
+        ('1A', '1ère Année'),
+        ('2A', '2ème Année'),
+        ('3A', '3ème Année'),
+    ]
+    
     id = models.AutoField(primary_key=True)
-    nom = models.CharField(max_length=100)
-    prenom = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, unique=True)
     role = models.CharField(max_length=100, choices=ROLES, default='utilisateur')
+    email = models.EmailField(unique=True)
+    prenom = models.CharField(max_length=30)
+    nom = models.CharField(max_length=30)
+    annee = models.CharField(max_length=2, choices=ANNEES, blank=True, null=True)
     username = None 
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = [
-        "nom",
-        "prenom",
-    ]
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['prenom', 'nom','role']
 
     objects = CustomUtilisateurManager()
 
     groups = models.ManyToManyField('auth.Group', related_name='custom_user_set', blank=True)
-    
     user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_permissions_set', blank=True)
     
     def __str__(self):
-        return "{0} {1}".format(self.nom, self.prenom)
+        return "{0} {1}".format(self.prenom, self.nom)
