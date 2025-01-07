5ebaa35c54779e1aaa0363faa60d3b2cf459fd92
QuentinDroucheau <quentin.droucheau@etu.univ-orleans.fr>
2025-01-07 14:36:16 +0100

    merge produits

--- apps/produits/views.py
+++ apps/produits/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# import pandas as pd
 from .models import Produit
+from django.contrib import messages
+from .forms import FileImportForm
 
 # Create your views here.
 
@@ -9,6 +12,36 @@ def produits(request):
         'titre': 'QuickLab',
         'produits': produits
     })
+
+# def importer(request):
+#     if request.method == 'POST':
+#         form = FileImportForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             fichier = request.FILES['fichier']
+#             if fichier.name.endswith('.csv'):
+#                 df = pd.read_csv(fichier)
+#             elif fichier.name.endswith('.xlsx'):
+#                 df = pd.read_excel(fichier)
+#             else:
+#                 messages.error(request, 'Format de fichier non supporté.')
+#                 return redirect('produits:importer_produits')
+
+#             for _, row in df.iterrows():
+#                 Produit.objects.create(
+#                     nom=row['nom'],
+#                     description=row['description'],
+#                     prix=row['prix'],
+#                     stock=row['stock']
+#                 )
+#             messages.success(request, 'Produits importés avec succès.')
+#             return redirect('produits:produits')
+#     else:
+#         form = FileImportForm()
+
+#     return render(request, 'produits/preparateurs/importer.html', {
+#         'titre': 'QuickLab',
+#         'form': form
+#     })
 
 def produit(request, produit_id):
     produit = Produit.objects.get(id=produit_id)
