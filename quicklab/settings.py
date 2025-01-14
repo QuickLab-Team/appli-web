0b291a3381ef1aed2acb6f4fde71a4cc092a2beb
QuentinDroucheau <quentin.droucheau@etu.univ-orleans.fr>
2025-01-14 16:35:43 +0100

    Fix de l'acces à une page lorsque l'utilisateur n'est pas connecté

--- quicklab/settings.py
+++ quicklab/settings.py
@@ -163,3 +163,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'utilisateurs:connexion'
+LOGIN_REDIRECT_URL = 'utilisateurs:accueil'
