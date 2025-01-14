6b443dba16f0b72e4b82b9940b65ebc7baa1f535
QuentinDroucheau <quentin.droucheau@etu.univ-orleans.fr>
2025-01-14 22:10:07 +0100

    Divers fix sur la réservation + fix css

--- apps/reservations/urls.py
+++ apps/reservations/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.reservations, name='reservations'),
     path('<int:reservation_id>/', views.detail_reservation, name='detail_reservation'),
     path('ajouter_message/<int:reservation_id>/', views.ajouter_message, name='ajouter_message_reservation'),
+    path('annuler/<int:reservation_id>/', views.annuler_reservation, name='annuler_reservation'),
 ]
