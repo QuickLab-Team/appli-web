a4835de14869ad4cbfd2dd2faad48f1b5caf0c68
IZUNA-coder <80841824+IZUNA-coder@users.noreply.github.com>
2025-01-15 12:09:24 +0100

    Ajout de la fonctionnalité d'ajout, de modification et de suppression de produits, ainsi que des améliorations de l'interface utilisateur

--- apps/produits/urls.py
+++ apps/produits/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('<int:produit_id>/', views.produit, name='produit'),
     path('ajout_panier/<int:produit_id>/', ajout_panier, name='ajout_panier'),
     path('importer/', views.importer_produits, name='importer_produits'),
-    path('produit_detail/<int:produit_id>/', views.produit_detail, name='produit_detail')
+    path('produit_detail/<int:produit_id>/', views.produit_detail, name='produit_detail'),
+    path('ajouter_produit/', views.ajouter_produit, name='ajouter_produit'),
+    path('modifier_produit/<int:produit_id>/', views.modifier_produit, name='modifier_produit'),
+    path('supprimer_produit/<int:produit_id>/', views.supprimer_produit, name='supprimer_produit'),
 ]
