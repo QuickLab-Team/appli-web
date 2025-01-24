ea2d574ecc1c313a2579f6e715b3a5d4ee2d5806
QuentinDroucheau <quentin.droucheau@etu.univ-orleans.fr>
2025-01-24 17:36:53 +0100

    Revert "Merge pull request #13 from QuickLab-Team/mdp_oublie"
    This reverts commit 49014f8306d96cd25e02fb7d0e9200d5a5bed9a1, reversing
    changes made to b4d0e061b28ef27f2d3ba385f254df7f53de1379.

--- quicklab/urls.py
+++ quicklab/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,9 +24,4 @@ urlpatterns = [
     path('produits/', include('produits.urls')),
     path('reservations/', include('reservations.urls')),
     path('panier/', include('paniers.urls')),
-    
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
