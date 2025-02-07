5f78207c08aedaa4cbff5d44dd12710148eb0a20
QuentinDroucheau <quentin.droucheau@etu.univ-orleans.fr>
2025-02-07 10:19:41 +0100

    AJout mdp oublié

--- apps/utilisateurs/urls.py
+++ apps/utilisateurs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import ConnexionView
+from django.contrib.auth import views as auth_views
 
 app_name = 'utilisateurs'
 
@@ -19,4 +20,8 @@ urlpatterns = [
     path('changer_mot_de_passe/', views.changer_mot_de_passe, name='changer_mot_de_passe'),
     path('deconnexion/', views.deconnexion, name='deconnexion'),
     path('statistiques/', views.statistiques, name='statistiques'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
